b2f968802ccad7560e943de8526fcf3cf87aa82f
OSCAR <owaka@uwaterloo.ca>
2021-04-15 13:23:36 -0400

    Updates

--- TwoSum.py
+++ TwoSum.py
@@ -1,5 +1,16 @@
-def two_sum(item,target):
     
+"""
+Given an array of integrs nums and an integer target, return the index position of the 
+two numbers such that they add up to target.
+
+-Each input would have exactly one solution, and you may not use the same element twice.
+- You can return the answer in any order.
+
+"""
+
+
+def two_sum(item,target):
+
     sieze = len(item)    
     true_index = 0
     
@@ -11,7 +22,36 @@ def two_sum(item,target):
         if( (item[index%sieze]+item[true_index]==target) & ((index%sieze)!=(true_index)) ):
             return index%sieze,true_index
       
-    
+     
 nums = [2,7,11,15]
 value = 9
-print(two_sum(nums,value))
+# print(two_sum(nums,value))
+
+
+def sumsum(array,value):
+    
+    sieze = len(array)-1
+
+    if(sieze<1):
+        return False
+
+    pointer=0
+    runner=1
+
+    while(pointer<sieze):
+
+        if(array[pointer]+array[runner]==value):
+            return pointer,runner
+        else:
+            runner+=1
+
+        if(runner==sieze+1 and pointer!=sieze-1):
+            pointer+=1
+            runner=pointer+1
+        
+        if(pointer==(sieze-1) and runner==sieze+1):
+            break
+
+    return False
+
+print(sumsum(nums,value))
