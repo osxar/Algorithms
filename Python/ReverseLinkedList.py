f59c9d1445c51b98e98d7825375c068e88dd5e1d
OSCAR <owaka@uwaterloo.ca>
2021-05-01 15:49:41 -0400

    Updates

---
+++ Python/ReverseLinkedList.py
@@ -0,0 +1,22 @@
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def reverseLinkedList(head):
+    
+    prev = None
+    current = head
+    succ = None
+
+    while(current):
+        succ = current.next
+
+        current.next = prev
+        prev=current
+        current = succ
+
+
+    return prev
